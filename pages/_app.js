d8ec10ccea34c60ef5bdc50b0b95377d118a62c2
cutlerwater <cutlerwater2@live.com>
2021-12-28 11:44:14 -0500

    fixing size issues

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,7 @@
-import '../styles/globals.css'
+import Head from "next/head";
+import "tailwindcss/tailwind.css";
+import Router from "next/router";
+import "../styles/global.css";
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
